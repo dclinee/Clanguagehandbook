e06ce255048e902df2a318c9d782f4eb44a5bc85
dclinee <dengchanglin8@outlook.com>
2021-02-14 14:45:25 +0800

    add add_variable.c

---
+++ Basics/add_variable.c
@@ -0,0 +1,19 @@
+//Write a program in C to add 10 and 20 and print the result.
+#include <stdio.h>
+
+int main(void)
+
+{
+	int a = 10;
+
+	int b = 20;
+
+	int sum;
+	// int sum = a + b;
+
+	sum = a + b;
+
+	printf("Sum: %d\n", sum);
+
+	return 0;
+}
