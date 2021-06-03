70d5591f0e67bc6c2c8596354e7de7fb0d613448
dclinee <dengchanglin8@outlook.com>
2021-06-03 22:01:08 +0800

    Add fopen.c

---
+++ Cstdlib/fopen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE *fd;
+	
+	fd = fopen("file.txt", "w+");
+
+	fprintf(fd, "%s %s %s %d", "We", "are", "in", 2021);
+
+	fclose(fd);
+
+	exit(0);
+}
