818023122e3af3065ba7c223f922654479941257
dclinee <dengchanglin8@outlook.com>
2021-06-27 16:32:23 +0800

    add array.c file

---
+++ Basics/array.c
@@ -0,0 +1,19 @@
+/* array.c */
+#include <stdio.h>
+
+int main(void)
+{
+	int n[10];
+	int i, j;
+
+	for(i = 0; i < 10; i++)
+	{
+		n[i] = i + 100;	
+	}
+
+	for(j = 0; j < 10; j++)
+	{
+		printf("Element[%d] = %d\n", j, n[j]);	
+	}
+	return 0;
+}
