a59cbd483ab1cf1ebd331e30c82996d2852d6da2
dclinee <dengchanglin8@outlook.com>
2021-11-06 19:55:11 +0800

    Add file_eof.c file

---
+++ cprimerplus/chapter08/file_echo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	int ch;
+	FILE *fp;
+	char fname[50];
+
+	printf("Enter the name of the file: ");
+	scanf("%s", fname);
+	fp = fopen(fname, "r");
+	if (fp == NULL)
+	{
+		printf("Failed to open file. Bye\n");
+		exit(1);
+	}
+
+	while(ch = getc(fp) != EOF)
+		putchar(ch);
+	fclose(fp);
+
+	return 0;
+}
