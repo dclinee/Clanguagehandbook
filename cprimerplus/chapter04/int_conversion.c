5c5ad89b7c60813fbaba80d4c8626c3aee7d561e
dclinee <dengchanglin8@outlook.com>
2021-11-13 21:31:56 +0800

    Add chapter03 conents

---
+++ cprimerplus/chapter04/int_conversion.c
@@ -0,0 +1,19 @@
+/* int_conversion.c */
+// 一些不匹配的整形转换
+
+#include <stdio.h>
+#define PAGES 336
+#define WORDS 65618
+int main(void)
+{
+
+  short num = PAGES;
+  short mnum = -PAGES;
+
+  printf("num as short and unsigned short: %hd %hu\n", num, num);
+  printf("-mnum as short and unsigned short: %hd %hu\n", mnum, mnum);
+  printf("num as int and char: %d %c\n", num, num);
+  printf("WORDS as int, short, char: %d %hd %c\n", WORDS, WORDS, WORDS);
+
+  return 0;
+}
