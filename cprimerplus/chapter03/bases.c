c408fde88b873db54f2de03a7c01e48a842dc4d9
dclinee <dengchanglin8@outlook.com>
2021-11-13 23:52:57 +0800

    Add bases.c

---
+++ cprimerplus/chapter03/bases.c
@@ -0,0 +1,10 @@
+/* bases.c */
+#include <stdio.h>
+int main(void)
+{
+
+  int x = 100;
+  printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
+  printf("dec = %d; octal = %#o; hex = %#x\n", x, x, x);
+  return 0;
+}
