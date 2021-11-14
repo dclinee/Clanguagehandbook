cb01544c95ecaa2b60662832e29da2bd2e352d02
dclinee <dengchanglin8@outlook.com>
2021-11-14 12:52:37 +0800

    Add c_primer_plus book chapter03 context code printf2.c and printf1.c

---
+++ cprimerplus/chapter03/printf2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+  unsigned int un = 3000000000;
+  short end = 200;
+  long big = 65537;
+  long long verybig = 12345678908642;
+
+  printf("un = %u and not %d\n", un, un);
+  printf("end = %hd and %d\n", end, end);
+  printf("big = %ld and not %hd\n", big, big);
+  printf("verybig = %lld and not %ld\n", verybig, verybig);
+
+  return 0;
+}
