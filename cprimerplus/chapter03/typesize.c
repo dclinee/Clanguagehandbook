d94d67e05d8b743340c1a0aba1bc798611250531
dclinee <dengchanglin8@outlook.com>
2021-11-17 23:06:43 +0800

    Add typesize.c file

---
+++ cprimerplus/chapter03/typesize.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Type int has a size of %zd bytes\n", sizeof(int));
+  printf("Type char has a size of %zd bytes\n", sizeof(char));
+  printf("Type long has a size of %zd bytes\n", sizeof(long));
+  printf("Type long long has a size of %zd bytes\n", sizeof(long long));
+  printf("Type double has a size of %zd bytes\n", sizeof(double));
+  printf("Type long double has a size of %zd bytes\n", sizeof(long double));
+  return 0;
+}
