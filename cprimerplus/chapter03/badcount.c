a139b3fe830f5ba3f0a129c60a7f67dd9a7ec39c
dclinee <dengchanglin8@outlook.com>
2021-11-18 23:30:23 +0800

    Add badcount.c file

---
+++ cprimerplus/chapter03/badcount.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+  int n = 4;
+  int m = 5;
+  float f = 7.0;
+  float g = 8.0;
+  printf("%d\n", n, m);
+  printf("%d %d %d\n", n, m);
+  printf("%d %d\n", f, g);
+
+  return 0;
+}
