c5f3286a1b368e6460cc04de7ede593db4f8ca3e
dclinee <dengchanglin8@outlook.com>
2022-04-01 22:02:26 +0800

    "Modified function_pointer_init_use.c file."

--- Advanced/function_pointer_init_use.c
+++ Advanced/function_pointer_init_use.c
@@ -1,3 +1,11 @@
+/* Initializing function pointer and using function pointer. */
+/* Note that function pointer syntax is flexible.
+ * it can either look like most other uses of pointer,
+ * whit & and *, or you may omit that part of syntax.
+ *This is similar to how arrays are treated, where a 
+ * bare array decays to a pointer, but you may also prefix
+ * the array & to request its address.
+ */
 #include <stdio.h>
 void my_int_func(int x)
 {
@@ -10,4 +18,8 @@ int main()
     foo = &my_int_func;
     foo(2);
     (*foo)(2);
+
+    printf("The address of function pointer foo is : %p\n", &foo);
+    printf("The value at the address of function pointer foo is : %p\n", foo);
+    printf("The address of function my_int_func is : %p\n", &my_int_func);
 }
