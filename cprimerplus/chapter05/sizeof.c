e00df5702df9d14c95c9f46365d813c91ea3c9f7
dclinee <dengchanglin8@outlook.com>
2022-04-04 22:13:27 +0800

    "Add sizeof.c file about typt of sizeof operator."

---
+++ cprimerplus/chapter05/sizeof.c
@@ -0,0 +1,13 @@
+/* sizeof.c */
+#include <stdio.h>
+
+int main(void)
+{
+  int n = 0;
+  size_t intsize;
+
+  intsize = sizeof(int);
+  printf("n = %d, n has %zd bytes; all ints have %zd bytes.\n", n, sizeof(n), intsize);
+
+  return 0;
+}
