52ac6fbc4867659a9bce7db9c6852de326aae968
dclinee <dengchanglin8@outlook.com>
2022-04-04 22:00:49 +0800

    "Add divide.c and division.c files."

---
+++ cprimerplus/chapter05/division.c
@@ -0,0 +1,13 @@
+/* division.c division */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int a = 2;
+  float b = -3.6;
+
+  printf("b/a = %f\n", b/a);
+  //printf("b%a = %f\n", b%a);
+  return 0;
+}
