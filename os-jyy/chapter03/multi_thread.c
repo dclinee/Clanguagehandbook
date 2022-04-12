2b778692f5fa9c70d941b5bc8b85ec9fe33c86fa
dclinee <dengchanglin8@outlook.com>
2022-04-13 01:35:30 +0800

    "Add computer_system_code_test_jyy and computer_system_pa about jyy."

---
+++ os-jyy/chapter03/multi_thread.c
@@ -0,0 +1,24 @@
+#include "thread.h"
+void Ta()
+{
+    while(1)
+    {
+        printf("a");
+    }
+}
+
+void Tb()
+{
+    while(1)
+    {
+        printf("b");
+    }
+}
+
+int main()
+{
+    create(Ta);
+    create(Ta);
+    create(Tb);
+    create(Tb);
+}
