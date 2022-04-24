0e09c238fa1f8f4a627e7b4c35375fd2aea72dbe
dclinee <dengchanglin8@outlook.com>
2022-04-24 15:33:02 +0800

    "Use pointer naotation for array in day_mon3.c file."

---
+++ cprimerplus/chapter10/day_mon3.c
@@ -0,0 +1,14 @@
+// day_mon3.c -- uses pointer notation
+#include <stdio.h>
+#define MONTHS 12
+int main(void) {
+  int days[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int index = 0;
+  for(index = 0; index < MONTHS; index++)
+    {
+      printf("Month %2d has %d days.\n", index +1, *(days + index));
+      // use a pointer to identify an individual element of an arrayOne
+      // and to obtain its value
+    }
+  return 0;
+}
