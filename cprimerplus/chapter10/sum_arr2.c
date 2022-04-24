2fb47d6cca6854a3480262fc8e790838f41be57b
dclinee <dengchanglin8@outlook.com>
2022-04-24 21:22:46 +0800

    "Modified sum_arr2.c file."

--- cprimerplus/chapter10/sum_arr2.c
+++ cprimerplus/chapter10/sum_arr2.c
@@ -1,24 +1,24 @@
 // sum_arr2.c -- the sum of the elements of an array with two pointers
 #include <stdio.h>
 #define SIZE 10
-int sum(int *start, int *end);
+int sump(int *start, int *end);
 
 int main(void) {
   int marbles[SIZE] = {20, 10, 5, 39, 4, 16, 19, 26, 31, 20};
   long answer = 0;
-  answer = sum(marbles, marbles + SIZE);
+  answer = sump(marbles, marbles + SIZE);
   printf("The total number of marbles is %ld.\n", answer);
 
   return 0;
 }
 /* use pointer arithmetic */
-int sum(int *start, int *end)
+int sump(int *start, int *end)
 {
   int total = 0;
   while(start < end)
     {
-      total += *start++; // add value to total
-      start++;          // advance pointer to next element
+      total += *(start++); // add value to total
+      //start++;          // advance pointer to next element
     }
   return total;
 }
