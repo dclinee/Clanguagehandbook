c91f77bb4f73077aac0e82f02bb68c2b584f5310
dclinee <dengchanglin8@outlook.com>
2022-04-27 22:16:45 +0800

    "Add cypher1.c file."

---
+++ cprimerplus/chapter07/cypher1.c
@@ -0,0 +1,20 @@
+// cpyher1.c
+#include <stdio.h>
+#define SPACE ' '
+int main(void)
+{
+  char ch;
+
+  ch = getchar();
+  while(ch != '\n')
+    {
+      if(ch == SPACE)
+        putchar(ch);
+      else
+        putchar(ch + 1);
+      ch = getchar();
+    }
+  putchar(ch);
+
+  return 0;
+}
