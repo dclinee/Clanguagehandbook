d5bf8ece6a6a28721e82b720d42d0de00d17b9d0
dclinee <dengchanglin8@outlook.com>
2022-04-30 23:44:25 +0800

    "Add program1.1.1.c."

---
+++ pointer_array_in_C/program1.1.1.c
@@ -0,0 +1,18 @@
+// program1.1.c
+#include <stdio.h>
+int j, k;
+int *ptr;
+int main(void)
+{
+  j = 1;
+  k = 2;
+  ptr = &k;
+
+  printf("\n");
+  printf("j has the value %d and is stored at %p\n", j, &j);
+  printf("k has the value %d and is stored at %p\n", k, &k);
+  printf("ptr has the value %p and is stored at %p\n",ptr, &ptr);
+  printf("The value of the integer pointed to by ptr is %d\n", *ptr);
+
+  return 0;
+}
