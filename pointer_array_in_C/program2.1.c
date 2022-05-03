6cd503674f021d3082b0a903b35ebb6623d65ca8
dclinee <dengchanglin8@outlook.com>
2022-05-03 18:48:15 +0800

    "Add program2.1.c and program2.2.c and program2.3.c."

---
+++ pointer_array_in_C/program2.1.c
@@ -0,0 +1,19 @@
+// program2.1.c
+#include <stdio.h>
+int my_array[] = {1, 23, 17, 4, -5, 100};
+int *ptr = NULL;
+int main(void)
+{
+  int i;
+  // point our pointer to the first element of the array
+  ptr = &my_array[0];
+
+  printf("\n\n");
+  for (i = 0; i < 6; i++)
+    {
+      printf("my_array[%d] = %d\n", i, my_array[i]);
+      printf("ptr + %d = %d\n", i, *(ptr + i));
+    }
+
+  return 0;
+}
