8dd815b7fd11c10d298d9b4184ae1492b56a1321
dclinee <dengchanglin8@outlook.com>
2022-06-03 15:43:17 +0800

    "Modified printout.c file."

--- cprimerplus/chapter04/printout.c
+++ cprimerplus/chapter04/printout.c
@@ -7,11 +7,13 @@ int main(void)
   int number = 7;
   float pies = 12.75;
   int cost = 7800;
+  int pc = 2 * 6;
 
   printf("The %d contestants ate %f berry pies.\n", number, pies);
   printf("The value of pi is %f.\n", PI);
   printf("Farewell! thou art too dear for my possessing.\n");
   printf("%c%d\n", '$', 2 * cost);
+  printf("Only %d%% sally's gribbles were edible.\n", pc);
 
   return 0;
 }
