87a2ad995170b30935af7044eb2e484af9939d1c
dclinee <dengchanglin8@outlook.com>
2022-06-03 17:09:19 +0800

    "Add intconv.c file."

---
+++ cprimerplus/chapter04/intconv.c
@@ -0,0 +1,16 @@
+/*intconv.c*/
+#include <stdio.h>
+#define PAGES 336
+#define WORDS 65618
+int main(void)
+{
+  short num = PAGES;
+  short mnum = -PAGES;
+
+  printf("num as short and unsigned short: %hd %hu\n", num, mnum);
+  printf("-num as short and unsigned short: %hd %hu\n", mnum, mnum);
+  printf("num as int and char: %d %c\n", num, num);
+  printf("WORDS as int, short and char: %d %hd %c\n", WORDS, WORDS, WORDS);
+
+  return 0;
+}
