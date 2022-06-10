fd9bc1849953c9372d70df5779e428edaff8d29e
dclinee <dengchanglin8@outlook.com>
2022-06-10 15:51:41 +0800

    "Add assignment.c and assignment.cpp and getinfo.cpp files."

---
+++ cplusplus/chapter02/assignment.cpp
@@ -0,0 +1,19 @@
+// assignment.cpp -- display assignment in cpp program
+
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+
+  int steinway;
+  int baldwin;
+  int yamaha;
+
+  yamaha = baldwin = steinway = 88;
+
+  cout << "yamaha = " << yamaha << endl;
+  cout << "baldwin = " << baldwin << endl;
+  cout << "steinway = " << steinway << endl;
+  return 0;
+}
