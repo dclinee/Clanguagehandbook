502f372e08cc4c4cdc16b1ed28b009174d16df0a
dclinee <dengchanglin8@outlook.com>
2022-06-14 22:08:09 +0800

    "Add compound_statement.c file."

---
+++ cprimerplus/chapter05/compound_statement.c
@@ -0,0 +1,17 @@
+// compound_statement.c
+#include <stdio.h>
+int main(void)
+{
+  int toes = 12; // assignment statement
+  printf("%d\n", toes); // function expression statement
+  ; // blank statement
+  // compound statement
+  int years = 0;
+  while((years) < 100)
+    {
+      int wisdom = wisdom * 1.05;
+      printf("%d %d\n", years, wisdom);
+      years = years + 1;
+    }
+  return 0;
+}
