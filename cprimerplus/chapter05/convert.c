3864abcc5465fe564e9e45b0caca24351dbe1027
dclinee <dengchanglin8@outlook.com>
2022-06-15 22:43:20 +0800

    "Add convert.c file."

---
+++ cprimerplus/chapter05/convert.c
@@ -0,0 +1,21 @@
+// convert.c
+#include <stdio.h>
+int main(void)
+{
+  char ch;
+  int i;
+  float f1;
+
+  f1 = i = ch = 'C';
+  printf("ch = %c, i = %d, f1 = %2.2f\n", ch, i, f1);
+  ch = ch + 1;
+  i = f1 + 2 * ch;
+  f1 = 2.0 * ch + i;
+  printf("ch = %c, i = %d, f1 = %2.2f\n", ch, i, f1);
+  ch = 1107;
+  printf("Now ch = %c\n", ch);
+  ch = 80.89;
+  printf("Now ch = %c\n", ch);
+
+  return 0;
+}
