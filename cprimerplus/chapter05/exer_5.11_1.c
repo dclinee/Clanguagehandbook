8f0a66a23158611c7208e289b29832a209410e6a
dclinee <dengchanglin8@outlook.com>
2022-06-18 16:06:19 +0800

    "Add exer_5.11_1.c file."

---
+++ cprimerplus/chapter05/exer_5.11_1.c
@@ -0,0 +1,13 @@
+// exer_5.11_1.c
+#include <stdio.h>
+int main(void)
+{
+  int a = (2 + 3) * 6;
+  int b = (12 + 6) / 2 * 3;
+  int c = (2 + 3) / 4;
+  int d = 3 + 2 * (7 / 2);
+
+  printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+  return 0;
+}
