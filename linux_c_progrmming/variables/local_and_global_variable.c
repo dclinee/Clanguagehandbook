7a1f0bb349df983d0b656ae6736ce69b88e8a718
dclinee <dengchanglin8@outlook.com>
2022-06-19 23:48:39 +0800

    "Add local_and_global_variable.c file."

---
+++ linux_c_progrmming/variables/local_and_global_variable.c
@@ -0,0 +1,24 @@
+// local_and_global_variable.c
+#include <stdio.h>
+#include <stdlib.h>
+
+// declarearation of global variable
+int i = 100;
+
+void func(int);
+int main(void)
+{
+  // declaration of local variable
+  int i = 3;
+  func(i);
+  {
+  // declaration of local variable
+    int i = 5;
+    printf("i = %d\n", i);
+  }
+  return 0;
+}
+void func(int i)
+{
+  printf("i = %d\n", i);
+}
