3a98a5bca5821935e46d465880ac463de15a43b1
dclinee <dengchanglin8@outlook.com>
2022-06-21 17:01:58 +0800

    "Add char_signed_unsigned.cpp and wide_char_type.cpp files."

---
+++ cplusplus/chapter03/wide_char_type.cpp
@@ -0,0 +1,28 @@
+// wide_char_type.cpp -- expand 8 bits char with 16 bits char
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+  // basics char
+  char aoa = 'p';
+  cout << "aoa = " << aoa << endl;
+  cout << "CHAR_BIT = " << CHAR_BIT << endl;
+  cout << "CHAR_MIN = " << CHAR_MIN << endl;
+  cout << "CHAR_MAX = " << CHAR_MAX << endl;
+
+  // width char
+  wchar_t bob = L'p'; // a wide-character constant
+  wcout << "bob = " << bob << endl;
+
+  wcout << L"tall" << endl; // outputing a wide-character string
+
+
+  // sizeof(char) vs sizeof(wchar_t)
+  cout << "sizeof(char) = " << sizeof(char) << endl;
+  wcout << "sizeof(wchar_t) = " << sizeof(wchar_t) << endl;
+
+
+  return 0;
+}
