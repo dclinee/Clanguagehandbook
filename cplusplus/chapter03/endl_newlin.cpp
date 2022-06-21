40c353806e902bb56c666917edffbfa216e9bbd6
dclinee <dengchanglin8@outlook.com>
2022-06-21 14:36:09 +0800

    "Add endl_newlin.cpp file."

---
+++ cplusplus/chapter03/endl_newlin.cpp
@@ -0,0 +1,19 @@
+// endl_newlin.cpp -- display endl and '\n'
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // long string with '\n'
+  cout << endl << endl << "What next?" << endl << "Enter a number:" << endl;
+  cout << "\n\nWhat next?\nEnter a number:\n";
+
+  // display number with endl
+  cout << 2022 << endl;
+
+  // the end of string with '\n'
+  cout << "hello world!\n";
+
+
+  return 0;
+}
