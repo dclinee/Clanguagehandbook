47240dc9384bff3adcae8f69be4b087c9a5559e1
dclinee <dengchanglin8@outlook.com>
2022-06-21 17:32:59 +0800

    "Add bool.cpp file."

---
+++ cplusplus/chapter03/bool.cpp
@@ -0,0 +1,19 @@
+// bool.cpp -- bool type and int type
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int ans = true; // ans assigned 1
+  int promise = false; // promise assigned 0
+  cout << "ans = " << ans << endl;
+  cout << "promise = " << promise << endl;
+
+  bool start = -100; // start assigned true
+  bool stop = 0; // stop assigned false
+  cout << "start = " << start << endl;
+  cout << "stop = " << stop << endl;
+
+  return 0;
+}
