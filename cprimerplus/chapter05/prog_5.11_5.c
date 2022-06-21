48090513e16447c6731bb24b3c23659166cdfbee
dclinee <dengchanglin8@outlook.com>
2022-06-21 22:25:28 +0800

    "Add prog_5.11_5.c file."

---
+++ cprimerplus/chapter05/prog_5.11_5.c
@@ -0,0 +1,22 @@
+// prog_5.11_5.c
+#include <stdio.h>
+void sum_money_days(int n);
+int main(void)
+{
+  int number;
+  printf("Enter a number for days: ");
+  scanf("%d", &number);
+  sum_money_days(number);
+  return 0;
+}
+
+void sum_money_days(int n)
+{
+  int count = 0;
+  int sum = 0;
+  while (count++ < n)
+    {
+      sum = sum + count;
+    }
+  printf("%d\n", sum);
+}
