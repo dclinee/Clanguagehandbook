0e030adca56da5260458f85576986be68bda8aa0
dclinee <dengchanglin8@outlook.com>
2022-06-24 17:11:03 +0800

    "Add float_constant.cpp file."

---
+++ cplusplusprimer/chapter03/float_constant.cpp
@@ -0,0 +1,19 @@
+// float_constant.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float number1 = 1.234f;  // a float constant
+  float number2 = 2.45E20F; // a float constant
+  double number3 = 2.345324E28; // a double constant
+  long double number4 = 2.2L; // a long double constant
+
+  cout << "number1 = " << number1 << endl;
+  cout << "number2 = " << number2 << endl;
+  cout << "number3 = " << number3 << endl;
+  cout << "number4 = " << number4 << endl;
+
+
+  return 0;
+}
