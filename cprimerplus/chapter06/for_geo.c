f7a466424d5e7f80974bfe134d0d5122ad53147b
dclinee <dengchanglin8@outlook.com>
2022-06-26 14:15:00 +0800

    "Add for_geo.c and for_wild.c files."

---
+++ cprimerplus/chapter06/for_geo.c
@@ -0,0 +1,11 @@
+// for_geo.c
+#include <stdio.h>
+int main(void)
+{
+  double debt;
+  for(debt = 100.0; debt < 150; debt = debt * 1.1)
+    {
+      printf("Your debt is now $%.2lf.\n", debt);
+    }
+  return 0;
+}
