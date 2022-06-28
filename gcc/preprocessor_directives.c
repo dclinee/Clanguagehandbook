bb987b20a290afccc262bce41ea0610c3f14af8a
dclinee <dengchanglin8@outlook.com>
2022-06-28 15:08:02 +0800

    "Add preprocessor_directives.c file."

---
+++ gcc/preprocessor_directives.c
@@ -0,0 +1,20 @@
+// preprocessor_directives.c -- introduce to CPP
+/*
+ The C preprocessor is a macro procesor that is used automatically by
+the C compiler to transform your program before actual compilation (
+Proprocessor direcives are executed before compilation.). It is called
+a macro processor because it allows you to define macros, which are brief
+abbreviations for longer constructs. A macro is a segment of code which
+is replaced by the value of macro. Macor is defined by #define directive.
+List of preprocessor directive:
+1. #include
+2. #define
+3. #undef
+4. #ifdef
+5. #ifndef
+6. #if
+7. #elif
+8. #else
+9. #endif
+10.#error
+11.#pragma*/
