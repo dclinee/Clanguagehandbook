2e2ea8a31972d18ee0f71160eca06095103f7f93
dclinee <dengchanglin8@outlook.com>
2022-06-28 16:38:02 +0800

    "Deleted hello and modified preprocessor_directives.c file."

--- gcc/preprocessor_directives.c
+++ gcc/preprocessor_directives.c
@@ -18,3 +18,92 @@ List of preprocessor directive:
 9. #endif
 10.#error
 11.#pragma*/
+/*The #include preprocessor directive is used to paste code of given file
+ into current file. It is used inclue system-define and user-defined header
+files. If iinclude file is not found, compiler renders error. It has There
+variants:
+1. #include <file>
+This variant is used for system header files. It searches for a file named
+file in a list of directories specified by you, then in a standard list of
+system directories.
+
+2. #include "file"
+This variant is used for header files of your own program. It searches for
+a file named file first in the current directory, then in the same directories
+used for system header files. The current directory is the directory of the
+current input file.
+
+3. #include anything else
+This variant is called a computed #include. Any #include directive whose
+argument does not fit the above two forms is a computed include.*/
+
+/*
+ Macro's(#define)
+Let's start with macro, as we discuss, a macro is a segment of code which is
+replaced by the value of macro. Macor is defined by #define directive.
+1. Object-like Macors
+The object-like macro is an identifier that is replaced by value. It is widely
+used to represent numeric constants.
+*/
+// For example
+#if 0
+#include <stdio.h>
+#define PI 3.14159
+
+int main(void)
+{
+  printf("%f\n", PI);
+
+  return 0;
+}
+#endif
+//Function-like Macros
+//The function-like macro looks function call. For example:
+//Proprocessor Formatting
+#if 0
+#include <stdio.h>
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define FOO 1020
+#undef FOO
+
+
+int main(void)
+{
+  printf("Minimum between 10 and 20 is: %d.\n", MIN(10, 20));
+  //  printf("Foo = %d\n", FOO); // using undef FOO is undeclared
+  // compile-time error
+
+  return 0;
+}
+#endif
+
+// For example
+#if 0
+#include <stdio.h>
+#define NUMBER 1
+
+int main(void)
+{
+#if NUMBER == 0
+  printf("Value of Number is: %d\n", NUMBER);
+#elif NUMBER == 1
+  printf("NUMBER == %d\n", NUMBER);
+#else
+  printf("Value of Number is non-__zero.");
+#endif
+  return 0;
+}
+#endif
+
+// For example #error
+#include <stdio.h>
+#ifndef __MATH_H
+#error First include then compile
+#else
+int main(void)
+{
+  float a;
+  a = sqrt(7);
+  printf("%f\n", a);
+}
+#endif
