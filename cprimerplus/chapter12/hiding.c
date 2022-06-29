d8092645c9feb10aa0951b204443a7efcfc7901b
dclinee <dengchanglin8@outlook.com>
2022-06-29 22:20:38 +0800

    "Add hiding.c file."

---
+++ cprimerplus/chapter12/hiding.c
@@ -0,0 +1,22 @@
+// hiding.c
+#include <stdio.h>
+int main(void)
+{
+  int x = 30;
+
+  printf("x in outer block: %d at %p\n", x, &x);
+  {
+    int x = 77;
+    printf("x in inner block: %d at %p\n", x, &x);
+  }
+  printf("x in outer block: %d at %p\n", x, &x);
+  while (x++ < 33)
+    {
+      int x = 100;
+      x++;
+      printf("x in while loop: %d at %p\n", x, &x);
+    }
+  printf("x in outer block: %d at %p\n", x, &x);
+
+  return 0;
+}
