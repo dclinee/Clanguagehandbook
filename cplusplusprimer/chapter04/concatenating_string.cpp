280244e69437f6016bd5d9c32823e17271533711
dclinee <dengchanglin8@outlook.com>
2022-06-30 12:03:06 +0800

    "Add concatenating_string.cpp file."

---
+++ cplusplusprimer/chapter04/concatenating_string.cpp
@@ -0,0 +1,14 @@
+// concatenating_string.cpp -- display concatenate string literals
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  cout << "I'd give my right arm to be" " a great violinist.\n";
+  cout << "I'd give my right arm to be a great violinist.\n";
+  cout << "I'd give my right ar"
+    "m to be a great villinist.\n";
+
+  return 0;
+}
