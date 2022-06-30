51255a986e8e22017e52d9a2165c75bed38d6637
dclinee <dengchanglin8@outlook.com>
2022-06-30 21:29:53 +0800

    "Add forc99. file."

---
+++ cprimerplus/chapter12/forc99.c
@@ -0,0 +1,20 @@
+// forc99.c
+#include <stdio.h>
+int main(void)
+{
+  int n = 8;
+
+  printf("   Initially, n = %d at %p\n", n, &n);
+  for (int n = 1; n < 3; n++)
+    printf("      loop 1: n = %d at %p\n", n, &n);
+  printf("After loop 1, n = %d at %p\n", n, &n);
+  for (int n = 1; n < 3; n++)
+    {
+      printf(" loop 2 index n = %d at %p\n", n, &n);
+      int n = 6;
+      printf("      loop 2: n = %d at %p\n", n, &n);
+      n++;
+    }
+  printf("After loop 2, n = %d at %p\n", n, &n);
+  return 0;
+}
