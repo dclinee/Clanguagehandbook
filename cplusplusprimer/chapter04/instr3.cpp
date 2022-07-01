cf6f4d97c0258e74aafe30dbfb72832909945aa8
dclinee <dengchanglin8@outlook.com>
2022-07-01 11:13:26 +0800

    "Add numstr.cpp file."

---
+++ cplusplusprimer/chapter04/instr3.cpp
@@ -0,0 +1,19 @@
+// instr3.cpp -- reading more than one word with get() & get()
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  const int ArSize = 20;
+  char name[ArSize];
+  char dessert[ArSize];
+
+  cout << "Enter your name:\n";
+  cin.get(name, ArSize).get(); // read string, newline
+  cout << "Enter your favorite dessert:\n";
+  cin.get(dessert, ArSize).get();
+  cout << "I have some deliciou " << dessert;
+  cout << " for you, " << name << ".\n";
+
+  return 0;
+}
