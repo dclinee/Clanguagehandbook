a65868f13538ea424886e9bdfada31095a86f3d2
dclinee <dengchanglin8@outlook.com>
2022-07-01 11:14:40 +0800

    "Modified numstr.cpp file."

--- cplusplusprimer/chapter04/numstr.cpp
+++ cplusplusprimer/chapter04/numstr.cpp
@@ -7,6 +7,7 @@ int main()
   cout << "What year was your house built?\n";
   int year;
   cin >> year;
+  cin.get();
   cout << "What is its street address?\n";
   char address[80];
   cin.getline(address, 80);
