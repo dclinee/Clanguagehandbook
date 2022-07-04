5b163189029aab4d5f537101d14c06638de105c3
dclinee <dengchanglin8@outlook.com>
2022-07-04 14:10:19 +0800

    "Add raw.cpp file."

--- cplusplusprimer/chapter04/raw.cpp
+++ cplusplusprimer/chapter04/raw.cpp
@@ -6,5 +6,6 @@ int main()
 {
   cout << R"(Jim "king" Tutt uses "\n" instead of endl.)" << "\n";
   cout << "Jim \"King\" Tutt uses \"\\n\" instead of endl." << "\n";
+  cout << R"+*("(who would't?)", she whispered.)+*" << endl;
   return 0;
 }
