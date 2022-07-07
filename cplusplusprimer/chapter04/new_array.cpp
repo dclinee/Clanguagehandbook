241c75dd9eff903f06055c0e81432a0bc3482351
dclinee <dengchanglin8@outlook.com>
2022-07-07 20:20:08 +0800

    Add new_array.cpp file.

---
+++ cplusplusprimer/chapter04/new_array.cpp
@@ -0,0 +1,16 @@
+// new_array.cpp -- create array with new
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int *psome = new int [10];     // get a block of 10 ints
+  delete [] psome;
+  /*
+   Don't use delete to free memory that new didn't allocte.
+  Don't use delete to free the smae block of memory twice in successsion.
+  Use delete [] if you used new [] to allocate an array.
+  Use delete (no brackets) if you used new to allocate a single entity.
+  It's safe to apply delete to the null pointer(nothing happens).*/
+  return 0;
+}
