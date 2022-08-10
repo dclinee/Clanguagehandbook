9b7677b5f1a03d470d0d2538f05d176b7b9b2fe1
dclinee <dengchanglin8@outlook.com>
2022-08-10 12:39:26 +0800

    Modified nogood.c file.

--- cprimerplus/chapter02/nogood.c
+++ cprimerplus/chapter02/nogood.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(void)
-  (
+{
    int n, int n2, int n3;
    n = 5;
    n2 = n * n;
@@ -10,4 +10,4 @@ int main(void)
    printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3);
 
    return 0;
-)
+}
