524c67837d57dd3d451f0d0531b880173096f233
dclinee <dengchanglin8@outlook.com>
2022-09-02 22:34:30 +0800

    Add elisp/while-loop.el file.

---
+++ elisp/while-loop.el
@@ -0,0 +1,13 @@
+;;; package -- while-loop.el
+;;; commentary:
+;;; Code:
+(setq animals '(tiger cheetah lion whale))
+(defun print-elements-of-list (animals)
+  "Print each element of the ANIMALS of its own."
+  (while animals
+    (progn
+      (print (car animals))
+      (setq animals (cdr animals)))))
+(print-elements-of-list animals)
+
+;;;  while loop.el ends here
