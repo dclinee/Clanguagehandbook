aa5782f4d9eb1b00fc4ec4023981eaafc1782269
dclinee <dengchanglin8@outlook.com>
2022-09-11 10:51:55 +0800

    Modified hello.c file.

--- Advanced/hello.c
+++ Advanced/hello.c
@@ -1,6 +1,12 @@
 // hello.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
