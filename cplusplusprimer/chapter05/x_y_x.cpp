f5027dc605cf6e556614b0610f14dba0bb326237
dclinee <dengchanglin8@outlook.com>
2022-11-10 21:18:04 +0800

    Add x_y_x.cpp file and delete num_test and forloop files.

---
+++ cplusplusprimer/chapter05/x_y_x.cpp
@@ -0,0 +1,12 @@
+// x_y_z.cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x, y, z;
+  x = y = z = 0;
+  cout << "x = " << x << endl;
+  cout << "y = " << y << endl;
+  cout << "z = " << z << endl;
+  return 0;
+}
