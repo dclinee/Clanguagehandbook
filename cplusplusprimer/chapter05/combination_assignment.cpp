3be3ff12f37332b859c53b82c5b6f0b5ec8ca576
dclinee <dengchanglin8@outlook.com>
2022-11-15 19:14:46 +0800

    Add combination_assignment.cpp and delete order_evalution files.

---
+++ cplusplusprimer/chapter05/combination_assignment.cpp
@@ -0,0 +1,21 @@
+// combination_assignment.cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+  int k = 5;
+  k += 3;
+  cout << "k = " << k << endl;
+
+  int *pa = new int[10];
+  pa[4] = 12;
+  pa[4] += 6;
+  cout << "pa[4] = " << pa[4] << endl;
+  *(pa + 4) += 7;
+  cout << "pa[4] = " << pa[4] << endl;
+  pa[2] = 12;
+  pa += 2;
+  cout << "pa[2] = " << pa[2] << endl;
+
+  return 0;
+}
