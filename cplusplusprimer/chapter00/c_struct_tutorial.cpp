d95081dc2448daecf0a81f0dd39bfcd88c34f737
dclinee <dengchanglin8@outlook.com>
2023-01-23 12:40:10 +0800

    Modified c_struct_tutorial.cpp file.

--- cplusplusprimer/chapter00/c_struct_tutorial.cpp
+++ cplusplusprimer/chapter00/c_struct_tutorial.cpp
@@ -13,10 +13,19 @@ struct person {
 
 int main(void)
 {
+  // Create Structure variables
   struct person dclinee = {"dclinee", 18, 30000.0};
+  struct person *ptr = &dclinee;
+
+  //Access member of a structure with . member operator
   cout << "dclinee.name = " << dclinee.name << endl;
   cout << "dclinee.age = " << dclinee.age << endl;
   cout << "dclinee.salary = " << dclinee.salary << endl;
+  cout << "********************************************" << "\n";
+  //Access member of a structure with -> structure pointer operator
+  cout << "dclinee.name = " << ptr->name << endl;
+  cout << "dclinee.age = " << ptr->age << endl;
+  cout << "dclinee.salary = " << ptr->salary << endl;
 
   return 0;
 }
