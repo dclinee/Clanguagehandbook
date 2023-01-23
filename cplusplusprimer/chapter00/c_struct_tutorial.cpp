635b3c4ecb222f57df1ecb6b75b800cf9bbad70f
dclinee <dengchanglin8@outlook.com>
2023-01-23 14:19:00 +0800

    Add c_struct_tutorial and modified c_struct_tutorial.cpp file.

--- cplusplusprimer/chapter00/c_struct_tutorial.cpp
+++ cplusplusprimer/chapter00/c_struct_tutorial.cpp
@@ -10,6 +10,33 @@ struct person {
   int age;
   float salary;
 };
+// Array 0f structure
+/*
+ We can also declare an array of structure variables. in which each
+element of the array.*/
+struct Employee{
+  char ename[10];
+  int sal;
+};
+struct Employee emp[5];
+int i, j;
+void ask(void)
+{
+  for (i = 0; i < 3; i++)
+    {
+    printf("\nEnter %dst Employee record:\n", i + 1);
+    printf("\nEmployee name:\t");
+    scanf("%s", emp[i].ename);
+    printf("\nEnter Salary:\t");
+    scanf("%d", &emp[i].sal);
+  }
+  printf("\nDisplaying Emploee record:\n");
+  for (i = 0; i < 3; i++)
+    {
+      printf("\nEmployee name is %s", emp[i].ename);
+      printf("\nSalary is %d", emp[i].sal);
+}
+}
 
 int main(void)
 {
@@ -26,6 +53,8 @@ int main(void)
   cout << "dclinee.name = " << ptr->name << endl;
   cout << "dclinee.age = " << ptr->age << endl;
   cout << "dclinee.salary = " << ptr->salary << endl;
+
+  ask();
 
   return 0;
 }
