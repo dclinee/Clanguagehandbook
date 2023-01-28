8c8b61595a6a17199ba0a13929009672b8aa3de8
dclinee <dengchanglin8@outlook.com>
2023-01-28 17:48:35 +0800

    Modified interactive.el file.

--- elisp/interactive.el
+++ elisp/interactive.el
@@ -5,3 +5,15 @@
   (message "Hello World!"))
 ;; using C-x C-e load function hello()
 ;; M-x hello -> echo area output: Hello World!
+
+;; With interactive reads a string argument
+(defun hello-someone (someone)
+  "Say hello to SOMEONE via M-x hello-someone."
+  (interactive "sWho do you want to say hello to? ")
+  (message "Hello %s!" someone))
+;; To use multiple arguments, separate their 'interactive' entries with a newline
+(defun multiple-hello (someone num)
+  "Say hello to SOMEONE via M-x multiple-hello, for NUM times."
+  (interactive "sWho do you want to say hello to? \nnHow many times? ")
+  (dotimes (i num)
+    (insert (format "Hello %s!\n" someone))))
