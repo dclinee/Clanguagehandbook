a97b460fa7c2581854facca7858a8db197c84e53
dclinee <dengchanglin8@outlook.com>
2023-02-08 11:24:11 +0800

    Modified ptrdiff_t.c file.

--- pointer_array_in_C/ptrdiff_t.c
+++ pointer_array_in_C/ptrdiff_t.c
@@ -1,14 +1,33 @@
-// ptrdiff_t.c -- contained the result of two points sub opteration
+/* ptrdiff_t.c -- ptrdiff_t is a type of variable store the result of
+  subtracting and adding pointers. */
 #include <stdio.h>
-#include <string.h>
-
-int main(void)
+#include <stddef.h>
+#include <stdint.h>
+/*
+void add(void)
+{
+  int arr[4];
+  ptrdiff_t diff = (&arr[1]) + (&arr[3]);
+  printf("The result of adding is: %d\n", diff);
+}
+*/
+void info(void)
+{
+  printf("PTRDIFF_MAX = %ld\n", PTRDIFF_MAX);
+}
+void sub(void)
 {
   char arr[5] = {1, 2, 3, 4, 5};
   char *ptr = arr;
 
-  printf("%d\n", &arr[4] - &arr[0]);
-  printf("%d\n", &arr[3] - &arr[1]);
+  printf("The result of subtracting is: %ld\n", &arr[4] - &arr[0]);
+  printf("The result of subtracting is: %ld\n", &arr[0] - &arr[1]);
+}
+int main(void)
+{
+  // add();
+  info();
+  sub();
 
   return 0;
 }
