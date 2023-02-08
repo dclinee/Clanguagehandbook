8ed31a3b1a422d1aeb717b30930f9d0194d3c3c0
dclinee <dengchanglin8@outlook.com>
2023-02-08 10:43:38 +0800

    Modified ptrdiff_t.c file.

--- pointer_array_in_C/ptrdiff_t.c
+++ pointer_array_in_C/ptrdiff_t.c
@@ -1,5 +1,6 @@
 // ptrdiff_t.c -- contained the result of two points sub opteration
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -7,6 +8,7 @@ int main(void)
   char *ptr = arr;
 
   printf("%d\n", &arr[4] - &arr[0]);
+  printf("%d\n", &arr[3] - &arr[1]);
 
   return 0;
 }
