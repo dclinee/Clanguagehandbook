de22f4fff72cfed9a7f93db7da287263c1419666
dclinee <dengchanglin8@outlook.com>
2023-02-21 11:25:59 +0800

    Add forever_loop.cpp file.

---
+++ cplusplusprimer/chapter05/forever_loop.cpp
@@ -0,0 +1,15 @@
+// forever_loop.cpp
+#include <iostream>
+using namespace std;
+int main(void)
+{
+  int I = 0;
+  I++;
+  for (int i = 0; i < 30; i++)
+    {
+      cout << "hello world.\n";
+      if (30 >= I)
+    break;
+    }
+
+}
