ce2b669cf359e07864a33f37b914b0e27dc04817
dclinee <dengchanglin8@outlook.com>
2023-03-10 00:19:45 +0800

    Add if.cpp file.

---
+++ cplusplusprimer/chapter06/if.cpp
@@ -0,0 +1,22 @@
+// if.cpp -- using if statement
+#include <iostream>
+int main()
+{
+  using std::cin;      // using declarations
+  using std::cout;
+  char ch;
+  int spaces = 0;
+  int total = 0;
+  cin.get(ch);
+  while(ch != '.')
+    {
+      if (ch == ' ')
+        ++spaces;
+      ++total;
+      cin.get(ch);
+    }
+  cout << spaces << " spaces, " << total ;
+  cout << " characters total in sentence\n";
+
+  return 0;
+}
