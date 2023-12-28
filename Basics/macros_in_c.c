5247633c49522f0c6045421a4b083b04163acedb
dclinee <dengchanglin8@outlook.com>
2023-12-28 14:24:25 +0800

    Add macros_in_c.c file.

---
+++ Basics/macros_in_c.c
@@ -0,0 +1,14 @@
+// C program to illustrate macros
+#include <stdio.h>
+
+// Macro definition
+#define LIMIT 100
+
+// Driver Code
+int main(void)
+{
+  // Print the value of macro defined
+  printf("The value of LIMIT is %d.\n", LIMIT);
+
+  return 0;
+}
