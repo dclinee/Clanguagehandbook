7d349d0f88ab722bd0fce4f261e46e2c09aaf664
dclinee <dengchanglin8@outlook.com>
2025-07-08 16:16:06 +0800

    First program named hello_world.rs

---
+++ rust/hello_world/target/debug/deps/hello_world-00489a442c752968.d
@@ -0,0 +1,5 @@
+/Users/mrc20/Desktop/Clanguagehandbook/rust/hello_world/target/debug/deps/hello_world-00489a442c752968: src/main.rs
+
+/Users/mrc20/Desktop/Clanguagehandbook/rust/hello_world/target/debug/deps/hello_world-00489a442c752968.d: src/main.rs
+
+src/main.rs:
